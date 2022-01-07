339fccb0031d20ca10ef7c825d2a60491fd542e3
MDava <pooldaimon94@gmail.com>
2022-01-07 17:54:20 +0100

    Add files via upload

--- Day4/Advent/advent/src/main/java/com/advent/App.java
+++ Day4/Advent/advent/src/main/java/com/advent/App.java
@@ -3,8 +3,6 @@ import java.io.*;
 import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.*;
-
 /**
  * Hello world!
  *
@@ -25,33 +23,40 @@ public class App
             filein = new BufferedReader(new FileReader("dati.txt"));
             riga = filein.readLine();
             Numbers = new ArrayList<String>(Arrays.asList(riga.split(",")));
-
+            riga = filein.readLine();
+            contaRiga = -1; 
             while(riga != null){
                 riga = filein.readLine();
                 
                 if(!(riga.isBlank())){
                     contaRiga++;
                     rigaBoard = new ArrayList<String>(Arrays.asList(riga.split(",")));
-                    Board.add(contaRiga, rigaBoard);
-                    Boards.add(contaStrato, Board);
+                    Board.add(contaRiga, rigaBoard); 
                 }
                 else{
-                    Board = new ArrayList<>(); 
-                    contaRiga = -1;
-                    Boards.add(new ArrayList<ArrayList<String>>());
                     contaStrato++;
+                    Boards.add(contaStrato, Board);
+                    Board = new ArrayList<>(); 
+                    contaRiga = -1;                                
                 }
             }
-            
-
+            System.out.print(Numbers);
+            System.out.println(Boards.size());
+            System.out.println(Boards);
             filein.close();
             
+          
+            for(int i=0; i<Boards.size(); i++){
+                for(int j=0; j<Boards.get(i).size(); j++){
+                    for(int x=0; x<Boards.get(i).get(j).size(); x++){
+                        System.out.print(Boards.get(i).get(j).get(x));
+                    }        
+                }        
+            }
             
         } catch (Exception e) {
             //TODO: handle exception
         }
-        System.out.println(Numbers);
-        System.out.print(Boards);
 
     }
 }
